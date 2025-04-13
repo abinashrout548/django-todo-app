78f055f6e8bceae9f3dd892b9deb1d500b42f4bf
Abinash Rout <your@email.com>
2025-04-13 21:51:39 +0530

    Initial commit - Django To-Do App

---
+++ todo/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'todo'  # 👈 Add this line
+
+    def __str__(self):
+        return self.title
+
---
+++ todo/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+# todo/urls.py
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add_task, name='add_task'),
+    path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+]
